d8efb26f931fde43c60f13677bdc9b9368f8bf51
Amitabh Phule <amitabhphule4@gmail.com>
2025-12-11 10:39:37 +0530

    add sub

---
+++ arithmatic.py
@@ -0,0 +1,15 @@
+def add(a, b):
+    """Return a + b"""
+    return a + b
+
+def sub(a, b):
+    """Return a - b"""
+    return a - b
+
+def mul(a, b):
+    """Return a * b"""
+    return a * b
+
+def div(a, b):
+    """Return a / b (raises ZeroDivisionError on b == 0)"""
+    return a / b
--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+import arithmatic as am
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Sum is:", am.add(a, b))
+print("subtract is:", am.sub(a, b))
 
-print("hello world")
+
